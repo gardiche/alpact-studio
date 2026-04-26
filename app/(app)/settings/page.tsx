d4239f6680cc4e9eda3f4ced79f79295e1af6ad4
Thomas <gardet.thomas@gmail.com>
2026-04-26 22:26:51 +0200

    fix: cast settings form to User type

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -58,7 +58,7 @@ export default function SettingsPage() {
   function debouncedSave(data: typeof form) {
     setSaving(true);
     setTimeout(() => {
-      setUser({ ...user!, ...data, team_size: data.team_size ? parseInt(data.team_size) : null });
+      setUser({ ...user!, ...data, team_size: data.team_size ? parseInt(data.team_size) : null } as typeof user!);
       setSaving(false);
       setSaved(true);
       setTimeout(() => setSaved(false), 2000);
